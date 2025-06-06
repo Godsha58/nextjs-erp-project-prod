f02766c266b413ca67967db4d17ae6ec0a470953
Mariann Valdez Venegas <mavave3942@gmail.com>
2025-06-06 15:59:51 -0600

    Style: Mejora de interfaz en finance/page

--- app/finance/page.tsx
+++ app/finance/page.tsx
@@ -1,25 +1,65 @@
 "use client";
 
-import {FaArrowRight} from "react-icons/fa";
-import Card from "@/components/Card";
+import { FaArrowRight } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+import { FaFileInvoiceDollar } from "react-icons/fa6";
+import { FaClipboardCheck } from "react-icons/fa6";
+import { FaHandHoldingUsd } from "react-icons/fa";
 
-import { useRouter } from 'next/navigation';
-import styles from "./page.module.css"; 
+
+import Card from "@/components/Card";
+  
+
+import { useRouter } from "next/navigation";
 export default function FinancePage() {
   const router = useRouter();
 
   return (
-    <main className={`${styles.div_principal}`}>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <main className={`justify-center items-center p-6 `}>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <Card
-          label="Pending to Pay"
+          icon={<FaDollarSign className="w-10 h-10" />}
+          label="Pending to Pay" 
           buttonLabel={
             <div className="flex items-center gap-2">
               <FaArrowRight className="w-4 h-4" />
               <span>View Details</span>
             </div>
           }
-          onButtonClick={() => router.push('/finance/pending-to-pay')}
+          onButtonClick={() => router.push("/finance/pending-to-pay")}
+        />
+        <Card
+          icon={<FaClipboardCheck className="w-10 h-10" />}
+          label="Orders"
+          buttonLabel={
+            <div className="flex items-center gap-2">
+              <FaArrowRight className="w-4 h-4" />
+              <span>View Details</span>
+            </div>
+          }
+          onButtonClick={() => router.push("/finance/orders")}
+        />
+        <Card
+          icon={<FaFileInvoiceDollar className="w-10 h-10" />}
+          label="Invoices"
+          buttonLabel={
+            <div className="flex items-center gap-2">
+              <FaArrowRight className="w-4 h-4" />
+              <span>View Details</span>
+            </div>
+          }
+          onButtonClick={() => router.push("/finance/invoices")}
+        />
+        <Card
+          icon={<FaHandHoldingUsd className="w-10 h-10" />}
+          label="Payments Due"
+          buttonLabel={
+            <div className="flex items-center gap-2">
+              <FaArrowRight className="w-4 h-4" />
+              <span>View Details</span>
+            </div>
+          }
+          onButtonClick={() => router.push("/finance/payments-due")}
         />
       </div>
     </main>
